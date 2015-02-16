c3ca27b53549b0c1ce2ef841634822f7934746ed
Kini <kinka96@abv.bg>
2015-02-16 14:07:24 +0200

    7klip 7:38

--- server/config/routes.js
+++ server/config/routes.js
@@ -22,6 +22,11 @@ module.exports = function(app) {
 	app.post('/login', auth.login);
 	app.post('/logout', auth.logout);
 
+	app.get('/api/*', function(req, res){
+		res.status(404);
+		res.end();
+	})
+
 	app.get('*', function(req, res) {
 		res.render('index', {currentUser: req.user});
 	});
